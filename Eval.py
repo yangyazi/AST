a20407483c79fa0c36498b1f97a6c8be90c005ab
yangyazi <1159466418@qq.com>
2023-11-17 14:10:08 +0800

    修改eval

--- Eval.py
+++ Eval.py
@@ -1,17 +1,28 @@
 import argparse
 import os
+from pathlib import Path
+
 import torch
 import torch.nn as nn
 from PIL import Image
-from os.path import basename
-from os.path import splitext
 from torchvision import transforms
 from torchvision.utils import save_image
 import net
 
 
-def test_transform():
+# def test_transform():
+#     transform_list = []
+#     transform_list.append(transforms.ToTensor())
+#     transform = transforms.Compose(transform_list)
+#     return transform
+
+
+def test_transform(size, crop):
     transform_list = []
+    if size != 0:
+        transform_list.append(transforms.Resize(size))
+    if crop:
+        transform_list.append(transforms.CenterCrop(size))
     transform_list.append(transforms.ToTensor())
     transform = transforms.Compose(transform_list)
     return transform
@@ -25,12 +36,30 @@ parser.add_argument('--style', type=str, default = 'input/style/1.jpg',
                     help='File path to the style image, or multiple style \
                     images separated by commas if you want to do style \
                     interpolation or spatial control')
+
+#######
+parser.add_argument('--content_dir', type=str,default = r'E:\00propropropro\pytorch-AdaIN-master\pytorch-AdaIN-master\input\content',
+                    help='Directory path to a batch of content images')
+parser.add_argument('--style_dir', type=str,default = r'E:\00propropropro\pytorch-AdaIN-master\pytorch-AdaIN-master\input\style',
+                    help='Directory path to a batch of style images')
+#######
+
+
 parser.add_argument('--steps', type=str, default = 1)
 parser.add_argument('--vgg', type=str, default = 'model/vgg_normalised.pth')
 parser.add_argument('--decoder', type=str, default = 'model/decoder_iter_160000.pth')
 parser.add_argument('--transform', type=str, default = 'model/transformer_iter_160000.pth')
 
 # Additional options
+parser.add_argument('--content_size', type=int, default=0,
+                    help='New (minimum) size for the content image, \
+                    keeping the original size if set to 0')
+parser.add_argument('--style_size', type=int, default=0,
+                    help='New (minimum) size for the style image, \
+                    keeping the original size if set to 0')
+parser.add_argument('--crop', action='store_true',
+                    help='do center crop to create squared image')
+
 parser.add_argument('--save_ext', default = '.jpg',
                     help='The extension name of the output image')
 parser.add_argument('--output', type=str, default = 'output',
@@ -41,6 +70,11 @@ parser.add_argument('--output', type=str, default = 'output',
 args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# os.environ['CUDA_VISIBLE_DEVICES'] = '4'
+# device = torch.device('cuda')
+# print(device)
+# print(os.environ['CUDA_VISIBLE_DEVICES'])
 
 if not os.path.exists(args.output):
     os.mkdir(args.output)
@@ -73,35 +107,56 @@ enc_5.to(device)
 transform.to(device)
 decoder.to(device)
 
-content_tf = test_transform()
-style_tf = test_transform()
+# content_tf = test_transform()
+# style_tf = test_transform()
 
-content = content_tf(Image.open(args.content))
-style = style_tf(Image.open(args.style))
+content_tf = test_transform(args.content_size, args.crop)
+style_tf = test_transform(args.style_size, args.crop)
 
-style = style.to(device).unsqueeze(0)
-content = content.to(device).unsqueeze(0)
+output_dir = Path(args.output)
+assert (args.content or args.content_dir)
+if args.content:
+    content_paths = [Path(args.content)]
+else:
+    content_dir = Path(args.content_dir)
+    content_paths = [f for f in content_dir.glob('*')]
 
-with torch.no_grad():
+# Either --style or --styleDir should be given.
+assert (args.style or args.style_dir)
 
-    for x in range(args.steps):
+if args.style:
+    style_paths = [Path(args.style)]
+else:
+    style_dir = Path(args.style_dir)
+    style_paths = [f for f in style_dir.glob('*')]
 
-        print('iteration ' + str(x))
-        
-        Content4_1 = enc_4(enc_3(enc_2(enc_1(content))))
-        Content5_1 = enc_5(Content4_1)
-    
-        Style4_1 = enc_4(enc_3(enc_2(enc_1(style))))
-        Style5_1 = enc_5(Style4_1)
-    
-        content = decoder(transform(Content4_1, Style4_1, Content5_1, Style5_1))
 
-        content.clamp(0, 255)
+for content_path in content_paths:
 
-    content = content.cpu()
-    
-    output_name = '{:s}/{:s}_stylized_{:s}{:s}'.format(
-                args.output, splitext(basename(args.content))[0],
-                splitext(basename(args.style))[0], args.save_ext
-            )
-    save_image(content, output_name)
+    for style_path in style_paths:
+        content = content_tf(Image.open(str(content_path)))
+        style = style_tf(Image.open(str(style_path)))
+
+        style = style.to(device).unsqueeze(0)
+        content = content.to(device).unsqueeze(0)
+        with torch.no_grad():
+            for x in range(args.steps):
+                print('iteration ' + str(x))
+
+                Content4_1 = enc_4(enc_3(enc_2(enc_1(content))))
+                Content5_1 = enc_5(Content4_1)
+
+                Style4_1 = enc_4(enc_3(enc_2(enc_1(style))))
+                Style5_1 = enc_5(Style4_1)
+
+                content = decoder(transform(Content4_1, Style4_1, Content5_1, Style5_1))
+
+                content.clamp(0, 255)
+
+            content = content.cpu()
+
+            output_name = output_dir / '{:s}_stylized_{:s}{:s}'.format(
+                content_path.stem, style_path.stem, args.save_ext)
+            save_image(content, str(output_name))
+
+####################################
